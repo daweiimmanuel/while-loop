87c220a72059403a5ba09dcfaf9ca5e9b527f95e
David Immanuel <daweiimmanuel@users.noreply.github.com>
2017-03-21 17:50:29 +0700

    Add while-loop.js

---
+++ while-loop.js
@@ -0,0 +1,15 @@
+var i = 1;
+while(i<=20){
+  if(i%2===0){
+    console.log(i+' - I love coding');
+  }
+  i++;
+}
+
+var j = 20;
+while(i>0){
+  if(i%2===0){
+    console.log(i+' - I will become fullstack developer');
+  }
+  i--;
+}
